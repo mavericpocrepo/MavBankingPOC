1b9b744418097eafebcae1d97bef01d84a3ed176
Yashaswi Bhardwaj <meyash14@gmail.com>
2023-06-27 10:54:59 +0530

    yash:27-june-2023:1046

--- auth-service/src/main/java/com/mav/authservice/clients/UserFeignClient.java
+++ auth-service/src/main/java/com/mav/authservice/clients/UserFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "user-service", url = "http://localhost:8080/api/users")
+@FeignClient(value = "user-service", url = "http://localhost:3015/api/users")
 public interface UserFeignClient {
     @GetMapping("/userByEmail")
     UserDetailsResponse getUserByEmail(@RequestParam String email);
